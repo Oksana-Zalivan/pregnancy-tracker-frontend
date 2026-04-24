0eb7a5aa96228de9baf4f26e89ff7378b786e0f5
Oksana Zalivan <ksyushamartinovska@gmail.com>
2026-04-24 21:22:56 +0300

    Add frontend dependencies

---
+++ src/app/api/auth/register/route.js
@@ -0,0 +1,29 @@
+export async function POST(req) {
+  try {
+    const body = await req.json();
+
+    const response = await fetch("http://localhost:3001/api/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    const data = await response.json();
+
+    return new Response(JSON.stringify(data), {
+      status: response.status,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        message: "Помилка сервера",
+      }),
+      { status: 500 }
+    );
+  }
+}
