3bedf10f9db27b2b490f78c075c2de099472ea43
Anora888 <anoramaomao@gmail.com>
2026-04-30 19:46:21 +0300

    add universal loader component

---
+++ src/components/shared/loader/Loader.tsx
@@ -0,0 +1,20 @@
+import styles from './Loader.module.css';
+import clsx from 'clsx';
+
+type LoaderProps = {
+  size?: 'sm' | 'md' | 'lg';
+  fullScreen?: boolean;
+};
+
+export const Loader = ({ size = 'md', fullScreen = false }: LoaderProps) => {
+  return (
+    <div
+      className={clsx(
+        styles.loaderWrapper,
+        fullScreen && styles.fullScreen
+      )}
+    >
+      <div className={clsx(styles.spinner, styles[size])} />
+    </div>
+  );
+};
