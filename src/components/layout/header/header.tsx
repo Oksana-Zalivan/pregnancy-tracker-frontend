08172f95c5427e284c3fb97ec2f8fb07cf240b76
Oleksii Marushchak <OleksiiMarushchak@outlook.com>
2026-05-01 21:47:39 +0100

    add basic wait sprite.svg

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -4,24 +4,28 @@ import Link from "next/link";
 import css from "./Header.module.css";
 
 type HeaderProps = {
-  onOpenMenu: () => void;
+    onOpenMenu: () => void;
 };
 
 export default function Header({ onOpenMenu }: HeaderProps) {
-  return (
-    <header className={css.header}>
-      <Link href="/" className={css.logo}>
-        Pregnancy Tracker
-      </Link>
+    return (
+        <header className={css.header}>
+            <div className={css.headerContainer}>
+                <Link href="/" className={css.headerLogo}>
+                    <svg className={css.headerLogo} width={84} height={36}>
+                        <use href="/icons/logo.svg"></use>
+                    </svg>
+                </Link>
 
-      <button
-        type="button"
-        aria-label="Open menu"
-        className={css.menuButton}
-        onClick={onOpenMenu}
-      >
-        ☰
-      </button>
-    </header>
-  );
+                <button
+                    type="button"
+                    aria-label="Open menu"
+                    className={css.menuButton}
+                    onClick={onOpenMenu}
+                >
+                    ☰
+                </button>
+            </div>
+        </header>
+    );
 }
