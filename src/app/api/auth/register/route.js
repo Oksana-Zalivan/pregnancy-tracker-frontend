cd70d0556c545c07e4466e92c1043ca3bacea97f
Oksana Zalivan <ksyushamartinovska@gmail.com>
2026-05-01 23:04:51 +0300

    fix: env setup before merge

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -2,13 +2,17 @@ export async function POST(req) {
   try {
     const body = await req.json();
 
-    const response = await fetch("http://localhost:3001/api/auth/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(body),
-    });
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/register`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+        credentials: "include",
+      }
+    );
 
     const data = await response.json();
 
