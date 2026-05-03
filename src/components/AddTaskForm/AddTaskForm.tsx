f38f22f5e75c9a1d56949b4201b7553d6fc4acad
Polina <yuriinnx@gmail.com>
2026-05-03 17:54:57 +0300

    add classes to tsx and css files

--- src/components/AddTaskForm/AddTaskForm.tsx
+++ src/components/AddTaskForm/AddTaskForm.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import css from "./AddTaskForm.module.css";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import type { FormikHelpers } from "formik";
 import { useMutation } from "@tanstack/react-query";
@@ -91,18 +92,18 @@ export default function AddTaskForm({ onClose }: AddTaskFormProps) {
         <>
             <Toaster />
             <Formik<FormValues> initialValues={initialValues} onSubmit={handleSubmit} validationSchema={Schema}>
-                <Form>
-                    <div>
-                        <label htmlFor={`${fieldId}-name`}>Назва завдання</label>
-                        <Field id={`${fieldId}-name`} name="name" type="text"></Field>
-                        <ErrorMessage component="span" name="name"></ErrorMessage>
+                <Form className={css.form}>
+                    <div className={css.formGroup}>
+                        <label className={css.formLabel} htmlFor={`${fieldId}-name`}>Назва завдання</label>
+                        <Field className={css.formField} id={`${fieldId}-name`} name="name" type="text"></Field>
+                        <ErrorMessage className={css.formError} component="span" name="name"></ErrorMessage>
                     </div>
-                    <div>
-                        <label htmlFor={`${fieldId}-date`}>Дата</label>
-                        <Field id={`${fieldId}-date`} name="date" type="date"></Field>
-                        <ErrorMessage component="span" name="date"></ErrorMessage>
+                    <div className={css.formGroup}>
+                        <label className={css.formLabel} htmlFor={`${fieldId}-date`}>Дата</label>
+                        <Field className={css.formField} id={`${fieldId}-date`} name="date" type="date"></Field>
+                        <ErrorMessage className={css.formError} component="span" name="date"></ErrorMessage>
                     </div>
-                    <button type="submit" disabled={mutation.isPending}>
+                    <button className={css.submitButton} type="submit" disabled={mutation.isPending}>
                         {mutation.isPending ? "Збереження..." : "Зберегти"}
                     </button>
                 </Form>
