1a1cc0d0535148edfa6f0f9693a7603231708a14
Oleksii Marushchak <OleksiiMarushchak@outlook.com>
2026-05-05 00:07:34 +0100

    add onclick

--- src/components/layout/AuthBar/AuthBar.tsx
+++ src/components/layout/AuthBar/AuthBar.tsx
@@ -7,11 +7,11 @@ export default function AuthBar() {
     return (
         <div className={css.authBarMenu}>
             
-        <div className="divider" />
-            <Link href="/auth/register" className={css.authBarButtonPink}>
+        <div className={css.divider} />
+            <Link href="/auth/register" className={css.authBarButtonPink} onClick={onNavigate}>
                 Зареєструватися
             </Link>
-            <Link href="/auth/login" className={css.authBarButtonGray}>
+            <Link href="/auth/login" className={css.authBarButtonGray} onClick={onNavigate}>
                 Увійти
             </Link >
         </div>
