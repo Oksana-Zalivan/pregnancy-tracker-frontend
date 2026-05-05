4eb46942bc3fc63d902f690276abfb48d5f84a05
Oleksii Marushchak <OleksiiMarushchak@outlook.com>
2026-05-05 13:19:01 +0100

    fix onNavigate

--- src/components/layout/UserBar/UserBar.tsx
+++ src/components/layout/UserBar/UserBar.tsx
@@ -1,41 +1,47 @@
-import Link from "next/link";
 import css from "./UserBar.module.css";
 
 type User = {
-  name: string;
-  email: string;
-  avatar?: string;
+    name: string;
+    email: string;
+    avatar?: string;
 };
 
 type UserBarProps = {
-  user: User;
-  onLogout: () => void;
+    user: User;
+    onLogout: () => void;
+    onNavigate?: () => void;
 };
 
-export default function UserBar({ user, onLogout }: UserBarProps) {
-  return (
-    <div className={css.userBar}>
-      <div className={css.divider}></div>
+export default function UserBar({ user, onLogout, onNavigate}: UserBarProps) {
+    return (
+        <div className={css.userBar}>
+            <div className={css.divider}></div>
 
-      <div className={css.userBarRow}>
-        <div className={css.Profile}>
-          <img
-            className={css.avatar}
-            src={user?.avatar || "/images/placeholder-avatar.jpg"}
-            alt="User Avatar"
-          />
-          <div className={css.avatarContent}>
-            <p className={css.userName}>{user?.name}</p>
-            <p className={css.userEmail}>{user?.email}</p>
-          </div>
+            <div className={css.userBarRow}>
+                <div className={css.Profile}>
+                    <img
+                        className={css.avatar}
+                        src={user?.avatar || "/images/placeholder-avatar.jpg"}
+                        alt="User Avatar"
+                    />
+                    <div className={css.avatarContent}>
+                        <p className={css.userName}>{user?.name}</p>
+                        <p className={css.userEmail}>{user?.email}</p>
+                    </div>
+                </div>
+
+                <button
+                    className={css.logoutLink}
+                    onClick={() => {
+                        onNavigate?.();
+                        onLogout();
+                    }}
+                >
+                    <svg height={24} width={24}>
+                        <use href="/images/sprite.svg#icon-exit"></use>
+                    </svg>
+                </button>
+            </div>
         </div>
-
-        <button className={css.logoutLink} onClick={onLogout}>
-          <svg height={24} width={24}>
-            <use href="/images/sprite.svg#icon-exit"></use>
-          </svg>
-        </button>
-      </div>
-    </div>
-  );
+    );
 }
